5101e3baa312576714fc6fa3aa055b7f98146110
Sarorian <shamowen5@gmail.com>
2023-09-29 22:15:01 -0600

    feat: NumberofEvents

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import EventList from './components/EventList';
 import CitySearch from './components/CitySearch';
+import NumberOfEvents from './components/NumberOfEvents';
 
 const App = () => {
   return (
     <div className="App">
+      <NumberOfEvents/>
       <CitySearch />
       <EventList/>
     </div>
---
+++ src/__tests__/NumberOfEvents.js
@@ -0,0 +1,24 @@
+import userEvent from '@testing-library/user-event';
+import NumberOfEvents from '../components/NumberOfEvents';
+import { render } from '@testing-library/react';
+
+describe('<NumberOfEvents /> Component', () => {
+  let NumberOfEventsComponent;
+  beforeEach(() => {
+    NumberOfEventsComponent = render(<NumberOfEvents />);
+  });
+
+  test('has a textbox', () => {
+    expect(NumberOfEventsComponent.queryByRole('textbox')).toBeInTheDocument();
+  });
+
+  test('default value of input textbox is 32', () => {
+    expect(NumberOfEventsComponent.queryByRole('textbox')).toHaveValue('32');
+  });
+
+  test('updates number of events when user types', async () => {
+    await userEvent.type(NumberOfEventsComponent.queryByRole('textbox'), 
+        '{backspace}{backspace}10');
+    expect(NumberOfEventsComponent.queryByRole('textbox')).toHaveValue('10');
+  });
+});
