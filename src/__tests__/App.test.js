164de390d77d02cb8644e4f00178518cc767436d
Sarorian <shamowen5@gmail.com>
2023-09-30 14:41:41 -0600

    feat: 4.5 code

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,5 +1,7 @@
-import { render } from '@testing-library/react';
+import { render, within } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
+import { getEvents } from '../api';
 
 describe('<App /> component', () => {
     let AppDOM;
@@ -16,4 +18,49 @@ describe('<App /> component', () => {
     test('render NumberOfEvents', () => {
         expect(AppDOM.querySelector('#number-of-events')).toBeInTheDocument();
       })
+});
+
+describe('<App /> integration', () => {
+  let AppDOM;
+  beforeEach(() => {
+    AppDOM = render(<App />).container.firstChild;
+  })
+
+  test('renders a list of events matching the city selected by the user', async () => {
+    const user = userEvent.setup();
+
+    const CitySearchDOM = AppDOM.querySelector('#city-search');
+    const CitySearchInput = within(CitySearchDOM).queryByRole('textbox');
+
+    await user.type(CitySearchInput, "Berlin");
+    const berlinSuggestionItem = within(CitySearchDOM).queryByText('Berlin, Germany');
+    await user.click(berlinSuggestionItem);
+
+    const EventListDOM = AppDOM.querySelector('#event-list');
+    const allRenderedEventItems = within(EventListDOM).queryAllByRole('listitem');   
+
+    const allEvents = await getEvents();
+    const berlinEvents = allEvents.filter(
+      event => event.location === 'Berlin, Germany'
+    );
+
+    expect(allRenderedEventItems.length).toBe(berlinEvents.length);
+    allRenderedEventItems.forEach(event => {
+      expect(event.textContent).toContain("Berlin, Germany");
+    });
+  });
+
+  test('renders the number of events matching the number selected by the user', async () => {
+
+    const NumberOfEventsDOM = AppDOM.querySelector('#number-of-events');
+    const NumberOfEventsInput =
+      within(NumberOfEventsDOM).queryByRole('textbox');
+
+    await userEvent.type(NumberOfEventsInput, '{backspace}{backspace}5');
+
+    const EventListDOM = AppDOM.querySelector('#event-list');
+    const allRenderedEventItems =
+      within(EventListDOM).queryAllByRole('listitem');
+    expect(allRenderedEventItems.length).toEqual(5);
+  });
 });
